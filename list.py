7760b779e8af5c3bf7525f6db64360c7ad34c834
Chizom <chizomreal@gmail.com>
2021-10-22 21:55:43 +0100

    list is added to example files

---
+++ list.py
@@ -0,0 +1,10 @@
+listOfCars = ["Toyota", "Lambo", "Lexus", "Tesla"]
+listOfNumbers = [1, 4, 6, 7]
+listOfBooleans = [True, False, False, True]
+a = ['Cars', 400, False, 60.08]
+
+createList = list(("apple", "pie", "mango", "rice"))
+
+print(createList)
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+
